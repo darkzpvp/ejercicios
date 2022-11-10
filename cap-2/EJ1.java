6f5e26337b8bb954cc87e0b49b4afa6f2b5e9a67
darkzpvp <victor1val@hotmail.es>
2022-11-10 22:49:45 +0100

    hola

---
+++ cap-2/EJ1.java
@@ -0,0 +1,19 @@
+public class Area {
+	public static void main (String[] args) {
+		
+int a
+int b
+int c
+a=2
+b=2
+c=3
+
+System.out.println(a, b, c);
+c=a
+a=a
+b=b
+System.out.println(a, b, c);
+
+        System.out.println(area);
+	}
+}
